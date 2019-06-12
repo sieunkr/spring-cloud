66840cef8e2fae7b240f827fef9d16af2d8f6556
sieunkim <sieunkr@gmail.com>
2019-06-13 00:04:14 +0900

    ㅇㅇ

--- spring-cloud-openfeign/src/main/java/com/example/demo/NaverOpenApiClient.java
+++ spring-cloud-openfeign/src/main/java/com/example/demo/NaverOpenApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "post-api", url = "${naver.openapi.url}", fallback = NaverOpenApiClientFallback.class)
 public interface NaverOpenApiClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/v1/search/blog.jn", produces = "application/json")
+    @RequestMapping(method = RequestMethod.GET, value = "/v1/search/blog.json", produces = "application/json")
     ResponseNaverBlog findBlogByQuery(@RequestHeader("X-Naver-Client-Id") String clientId,
                                 @RequestHeader("X-Naver-Client-Secret") String clientSecret,
                                 @RequestParam(name = "query") String query);
