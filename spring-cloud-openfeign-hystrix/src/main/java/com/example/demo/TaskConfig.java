9b68de3379898b30a96fe4a5f880899f2ee591f3
Eddy.Kim <sieunkr@gmail.com>
2019-09-28 19:21:21 +0900

    ㅇㅇ

---
+++ spring-cloud-openfeign-hystrix/src/main/java/com/example/demo/TaskConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class TaskConfig {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(100);
+        taskExecutor.setMaxPoolSize(200);
+        taskExecutor.setQueueCapacity(50);
+        taskExecutor.setThreadNamePrefix("Executor-");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
--- spring-cloud-openfeign-hystrix/src/main/java/com/example/demo/TestController.java
+++ spring-cloud-openfeign-hystrix/src/main/java/com/example/demo/TestController.java
@@ -1,26 +1,34 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
 public class TestController {
 
     private final CafeClient cafeClient;
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @GetMapping
     public List<String> getAll(){
 
+        log.info(Thread.currentThread().getName());
+
+        /*
         List<CompletableFuture<List<String>>> futureList = new ArrayList<>();
 
         //TODO:쓰레드 풀 설정
@@ -33,6 +41,20 @@ public class TestController {
                         .flatMap(Collection::stream)
                         .collect(Collectors.toList());
 
-        return items;
+        */
+
+        CompletableFuture futureByCoffee = CompletableFuture.supplyAsync(cafeClient::getCoffees, threadPoolTaskExecutor);
+        CompletableFuture futureByJuice = CompletableFuture.supplyAsync(cafeClient::getJuices, threadPoolTaskExecutor);
+
+        List<CompletableFuture<List<String>>> futureList =
+                Arrays.asList(futureByCoffee, futureByJuice);
+
+        return CompletableFuture.allOf(futureByCoffee, futureByJuice)
+                .thenApply(Void -> futureList.stream()
+                        .map(CompletableFuture::join)
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toList())
+                ).join();
+
     }
 }
